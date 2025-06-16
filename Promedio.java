9f2f5a9316dd367f89f60a8a66ccc130430c3c77
mildreth <202400896f@utea.edu.pe>
2025-06-16 09:34:49 -0500

    promedio

--- Promedio.java
+++ Promedio.java
@@ -5,7 +5,26 @@ public class Promedio {
         /* 
         *
         */
-        Scanner s= new Scanner(null)
+
+     Scanner s = new Scanner(System.in);
+        double prac, exp, inv, exa, act;
+        System.out.println("Ingrese las 5 notas separadas por un espacio");
+        prac = s.nextDouble();
+        exp = s.nextDouble();
+        inv = s.nextDouble();
+        exa = s.nextDouble();
+        act = s.nextDouble();
+
+        double promedio = (prac+exp+inv+exa+act)/5;
+
+        if (promedio >= 10.5) {
+            System.out.println("Aprobado con "+promedio);
+        } else {
+            System.out.println("Desaprobado con "+promedio);
+        }
+    }
+
+
     }
     
-}
+
